1268120f2533c74ecd349e3e325de53699ef75a0
TheJohny71 <jduval71@me.com>
2024-12-13 17:25:07 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // This ensures assets are properly loaded in GitHub Pages
+  assetPrefix: '/alfie-enterprise-v3/',
 }
 
 module.exports = nextConfig
