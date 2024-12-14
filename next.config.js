0f23745d8c7018fe49f0c5df8954a5e9db24224a
TheJohny71 <jduval71@me.com>
2024-12-13 18:06:36 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'out',
   basePath: '/alfie-enterprise-v3',
   images: {
     unoptimized: true,
   },
-  assetPrefix: '/alfie-enterprise-v3',
+  assetPrefix: '/alfie-enterprise-v3/',  // Note the trailing slash
   trailingSlash: true,
+  // Add this to help with chunk loading
+  webpack: (config) => {
+    config.output.publicPath = `/alfie-enterprise-v3/_next/`;
+    return config;
+  },
 }
 
 module.exports = nextConfig
